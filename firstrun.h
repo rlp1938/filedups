af192a869f4046ddd1d631cc15051c22f126ac12
bob <rlp1938@gmail.com>
2020-03-01 16:41:54 +1100

    Coppyright date changed.

--- firstrun.h
+++ firstrun.h
@@ -1,6 +1,6 @@
 /*
  * firstrun.h
- * Copyright 2019 Robert L (Bob) Parker rlp1938@gmail.com
+ * Copyright 2020 Robert L (Bob) Parker rlp1938@gmail.com
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
