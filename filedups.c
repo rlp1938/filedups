0c63b775e1eb98f684f2bc4bd440aa250a69b398
bob <rlp1938@gmail.com>
2020-03-11 22:39:28 +1100

    1. Writes selected file paths directly into memory, not via writing to file. 2. Altered user input to allow an indefinite number of chosen dirs to file paths to be tested.

--- filedups.c
+++ filedups.c
@@ -38,6 +38,12 @@
 #include <errno.h>
 #include <time.h>
 
+#include "str.h"
+#include "dirs.h"
+#include "files.h"
+#include "gopt.h"
+#include "firstrun.h"
+
 // structs
 typedef struct filerec_t {
   char *path;
@@ -52,20 +58,20 @@ typedef struct prgvar_t {
   char *files_list;
   filerec_t *list1;
   filerec_t *list2;
-  int lc1;  // record count of list1
-  int lc2;  // record count of list2
-  int pages;
+  int lc1;    // record count of list1
+  int lc2;    // record count of list2
+  int pages;  /* Max number of blocks of 4096 bytes to calculate
+  * md5sum for a file. If pages < 1 then the entire file size will be
+  * used. */
+  size_t dat_size;  // initial memory allocation for files_list.
+  size_t inc_size;  // If dat_size bytes is too small, add this value.
+  mdata *md;        // describes a block of chars in memory.
 } prgvar_t;
 
 typedef struct size_inode_t {
   size_t size;
   ino_t inode;
 } si_t;
-
-#include "dirs.h"
-#include "files.h"
-#include "gopt.h"
-#include "firstrun.h"
 
 // Globals
 static char *vsn;
@@ -76,39 +82,54 @@ static void
 dovsn(void);
 static void
 is_this_first_run(void);
+static prgvar_t
+*read_config(const char *prgname);
 static void
 free_prgvar_t(prgvar_t *pv);
-static prgvar_t
-*setup_program(options_t opt, int argc, char **argv);
-static char
-*validate_input(int argc, char **argv);
 static void
-fdrecursedir(const char *dirname, FILE *fpo);
+setup_program(options_t *opt, int argc, char **argv, prgvar_t *pv);
+static void
+validate_input(const char *path);
+static void
+fdrecursedir(const char *dirname, prgvar_t *pv);
 static int
 dname_grep(const char *dname);
-static char
-*make_files_list(const char *dir);
-static mdata
-*file_data_to_list(prgvar_t *pv);
+static void
+make_filerecord_list(prgvar_t *pv);
+static void
+make_files_list(prgvar_t *pv);
 static si_t
 *get_size_inode(const char *p);
 static void
-delete_unique_size(prgvar_t *pv);
+delete_unique_size_file_records(prgvar_t *pv);
 static int
 cmpinodep(const void *p1, const void *p2);
 static int
 cmpsize_inodep(const void *p1, const void *p2);
-
+static void
+mem_append(const char *p, prgvar_t *pv);
 
 int main(int argc, char **argv)
 { /* main */
   vsn = "1.0";
+  prgvar_t *pv = read_config("filedups");
   options_t opt = process_options(argc, argv);
-  prgvar_t *pv = setup_program(opt, argc, argv);
-  printf("%s\n", pv->dirpath);
-  pv->files_list = make_files_list(pv->dirpath);  // writes a file.
-  mdata *fd = file_data_to_list(pv);
-  delete_unique_size(pv);
+  setup_program(&opt, argc, argv, pv);
+  char thepath[PATH_MAX];
+  pv->lc1 = 0;  // redundant.
+  int i;
+  if (argc == 1) {
+    pv->dirpath = realpath("./", thepath);
+    make_files_list(pv);
+    printf("%s\n", pv->dirpath);
+  } else for (i = 1; argv[i] ; i++) {
+    pv->dirpath = realpath(argv[i], thepath);
+    validate_input(thepath);
+    make_files_list(pv);
+    printf("%s\n", pv->dirpath);
+  }
+  make_filerecord_list(pv);
+  delete_unique_size_file_records(pv);
   // delete the /tmp file.
   // free the files data block.
   return 0;
@@ -161,40 +182,44 @@ free_prgvar_t(prgvar_t *pv)
   free(pv);
 } // free_prgvar_t()
 
-static prgvar_t
-*setup_program(options_t opt, int argc, char **argv)
+static void
+setup_program(options_t *opt, int argc, char **argv, prgvar_t *pv)
 { /* If --help or --version is a chosen option, action those and quit,
    * otherwise check argv[1] for validity and set up those prgvar_t
    * variables that are known at this stage.
   */
-  if (opt.runhelp) dohelp(0); // will exit.
-  if (opt.runvsn) dovsn(); // will exit.
+  if (opt->runhelp) dohelp(0); // will exit.
+  if (opt->runvsn) dovsn(); // will exit.
   is_this_first_run();
-  prgvar_t *pv = xcalloc(1, sizeof(struct prgvar_t));
-  pv->dirpath = validate_input(argc, argv); // if error, no return.
-  return pv;
+  /* data size has been set from read_config(), alter these settings,
+   * alter these settings if options require it.
+  */
+  if (opt->dat_size) pv->dat_size = opt->dat_size;
+  if (opt->dat_incr) pv->inc_size = opt->dat_incr;
+  /* regardless of how the data size and it's increment were set,
+   * ensure that they are set to a memory page boundary. */
+  pv->dat_size += (pv->dat_size % 4096);
+  pv->inc_size += (pv->inc_size % 4096);
+  static mdata md;
+  md.fro = xcalloc(pv->dat_size, 1);
+  md.limit = md.fro + pv->dat_size;
+  md.to = md.fro; // data block is empty.
+  pv->md = &md;
 } // setup_program()
 
-static char
-*validate_input(int argc, char **argv)
+static void
+validate_input(const char *p)
 { /* test for sane user input, either one dir or none. Return
   valid dir path if it exists. */
-  static char p[PATH_MAX];
-  if (argc > 2) {
-    fprintf(stderr, "Input just one dir or by default none.\n");
-    exit(EXIT_FAILURE);
-  }
-  if (argv[1]) realpath(argv[1], p); else realpath("./", p);
   if (!exists_dir(p)) {
     fprintf(stderr, "Not a directory: %s\n", p);
     exit(EXIT_FAILURE);
   }
-  return p;
 } // validate_input()
 
 static void
-fdrecursedir(const char *path, FILE *fpo)
-{ /* Record eligible files in a temporary file. */
+fdrecursedir(const char *path, prgvar_t *pv)
+{ /* Record eligible files in a block of memory. */
   DIR *dp = opendir(path);
   if (!dp) {
     perror(path);
@@ -211,10 +236,11 @@ fdrecursedir(const char *path, FILE *fpo)
     strcat(joinbuf, de->d_name);
     switch (de->d_type) {
     case DT_DIR:
-      fdrecursedir(joinbuf, fpo);
+      fdrecursedir(joinbuf, pv);
       break;
     case DT_REG:
-      fprintf(fpo, "%s\n", joinbuf);
+      mem_append(joinbuf, pv);
+      pv->lc1++;
       break;
     default:
       break;  // no interest in anything except regular files and dirs.
@@ -233,27 +259,20 @@ dname_grep(const char *dname)
   return -1;
 } // dname_grep()
 
-static char
-*make_files_list(const char *dir)
-{ /* opens a temporary file and records the list of files in it. */
-  static char fn[NAME_MAX];
-  sprintf(fn, "/tmp/filedups%lu.lst", time(NULL));
-  FILE *fpo = dofopen(fn, "w");
-  fdrecursedir(dir, fpo);
-  dofclose(fpo);
-  return fn;
+static void
+make_files_list(prgvar_t *pv)
+{ /* writes file paths to a block of memory as C strings. */
+  fdrecursedir(pv->dirpath, pv);
 } // make_files_list()
 
-static mdata
-*file_data_to_list(prgvar_t *pv)
+static void
+make_filerecord_list(prgvar_t *pv)
 { /* The files to consider for duplication are recorded in a file;
    * they are to be included into a list of file records.
    * Returning the mdata pointer allows the data block to be free'd.
   */
-  mdata *fd = readfile(pv->files_list, 1, 0);
-  pv->lc1 = memlinestostr(fd);
   pv->list1 = xcalloc(pv->lc1, sizeof(struct filerec_t));
-  char *cp = fd->fro;
+  char *cp = pv->md->fro;
   int i;
   for (i = 0; i < pv->lc1; i++) {
     si_t *sit = get_size_inode(cp);
@@ -264,7 +283,6 @@ static mdata
       cp += strlen(cp) + 1;
     } // if()
   } // for()
-  return fd;
 } // file_data_to_list()
 
 static si_t
@@ -286,7 +304,7 @@ static si_t
 } // get_size_inode()
 
 static void
-delete_unique_size(prgvar_t *pv)
+delete_unique_size_file_records(prgvar_t *pv)
 { /* sort the list of file records on size and delete those having
    * singular size.
   */
@@ -321,7 +339,7 @@ delete_unique_size(prgvar_t *pv)
       j++;
     } // if()
   } // for()
-} // delete_unique_size()
+} // delete_unique_size_file_records()
 
 static int
 cmpinodep(const void *p1, const void *p2)
@@ -362,3 +380,47 @@ cmpsize_inodep(const void *p1, const void *p2)
   }
   return 0;
 } // cmpsize_inodep()
+
+static prgvar_t
+*read_config(const char *prgname)
+{ /* presently this is just a bullshit placeholder that acts as if it
+   * really did read a config file. */
+  static prgvar_t pv = {0};
+  static mdata md;
+  pv.dat_size = 1024 * 1024;  // 1 meg so far;
+  pv.inc_size = pv.dat_size / 10; // may be replaced by options.
+  pv.pages = 1; // option can vary this.
+  pv.md = &md;
+  return &pv;
+} // read_config()
+
+void
+mem_append(const char *p, prgvar_t *pv)
+{ /* appends a C string to a memory block, taking care of memory
+   * re-sizing as required. I build in an 8 byte safety margin before
+   * invoking a realloc() operation.
+  * */
+  size_t len = strlen(p);
+  size_t available = pv->md->limit - pv->md->to;
+  if ( len + 8 > available) { // realloc() required
+    size_t addon = pv->inc_size;
+    while (len + 8 > addon) addon += len; // not likely, but be sure.
+    // record current block parameters
+    char *fro = pv->md->fro;
+    char *to = pv->md->to;
+    char *limit = pv->md->limit;
+    size_t newsize = limit - fro + addon;
+    pv->md->fro = realloc(fro, newsize);
+    if (pv->md->fro != fro) {
+      off_t locdiff = pv->md->fro - fro;
+      to += locdiff;
+      limit += locdiff;
+    }
+    memset(limit, 0, addon);
+    limit += addon;
+    pv->md->to = to;
+    pv->md->limit = limit;
+  }
+  strcpy(pv->md->to, p);
+  pv->md->to += len + 1;
+} // mem_append()
